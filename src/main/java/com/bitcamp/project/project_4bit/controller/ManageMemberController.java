f1191725bcc965fd4579e5bfbb8c21bff7d6d01a
SEOYOUNG <seo089@naver.com>
2019-08-16 18:33:06 +0900

    merge update

--- src/main/java/com/bitcamp/project/project_4bit/controller/ManageMemberController.java
+++ src/main/java/com/bitcamp/project/project_4bit/controller/ManageMemberController.java
@@ -78,7 +78,7 @@ public class ManageMemberController {
 
     // classId 로 강사를 찾아오는 Controller
     // Class_Teacher_Log 테이블에 데이터가 들어가있어야 조회가 된다.
-    // EndPoint :
+    // EndPoint : http://localhost:8080/manage/member/teacher?classId={classId}
     @PreAuthorize("hasAnyAuthority('MEMBER_READ')")
     @RequestMapping(
             path = "/teacher",
@@ -92,7 +92,7 @@ public class ManageMemberController {
     }
 
     // classId 로 회원리스트를 찾아오는 컨트롤러
-    // EndPoint : http://localhost:8080/manage/member/student/list?classId=1
+    // EndPoint : http://localhost:8080/manage/member/student/list?classId={classId}
     @PreAuthorize("hasAnyAuthority('MEMBER_READ')")
     @RequestMapping(
             path = "/student/list",
@@ -114,7 +114,7 @@ public class ManageMemberController {
 
 
     // 회원 상세로 들어가는 컨트롤러
-    // EndPoint : http://localhost:8080/manage/member?userId=18
+    // EndPoint : http://localhost:8080/manage/member?userId={userId}
     @PreAuthorize("hasAnyAuthority('MEMBER_READ')")
     @RequestMapping(
             method = RequestMethod.GET,
@@ -147,7 +147,7 @@ public class ManageMemberController {
     }
 
     // admin이 user를 삭제
-    // endpoint : http://localhost:8080/manage/member?userId=17
+    // endpoint : http://localhost:8080/manage/member?userId={userId}
     @PreAuthorize("hasAnyAuthority('MEMBER_WRITE')")
     @RequestMapping(
             method = RequestMethod.DELETE,
