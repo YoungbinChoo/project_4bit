30e9f0f79c0d93561033eff56c52e14d75807a0f
someyou9292 <hoyaa92@gmail.com>
2019-08-25 14:33:18 +0900

    0825_ 중호의 일기 클래스 리스트 나오는거 구현

--- src/main/java/com/bitcamp/project/project_4bit/service/ProfileService.java
+++ src/main/java/com/bitcamp/project/project_4bit/service/ProfileService.java
@@ -3,6 +3,7 @@ package com.bitcamp.project.project_4bit.service;
 import com.bitcamp.project.project_4bit.entity.Admin;
 import com.bitcamp.project.project_4bit.entity.ClassTeacherLog;
 import com.bitcamp.project.project_4bit.entity.Student;
+import com.bitcamp.project.project_4bit.entity.Teacher;
 import com.bitcamp.project.project_4bit.repository.AdminRepository;
 import com.bitcamp.project.project_4bit.repository.ClassTeacherLogRepository;
 import com.bitcamp.project.project_4bit.repository.StudentRepository;
@@ -32,6 +33,7 @@ public class ProfileService {
         return adminRepository.findByUser_UserId(userId);
     }
 
+
     @Transactional
     public ClassTeacherLog selectTeacher(Long userId){
         // 1. 컨트롤러에서 전달받은 userId 로 teacher 테이블의 user의 userId 로 강사Id를 찾아 저장
@@ -40,6 +42,7 @@ public class ProfileService {
         // 2. 1에서 찾은 teacherId 를 이용하여 classTacherLog의 강사정보를 찾아 리턴
         return classTeacherLogRepository.findAllByTeacher_TeacherId(teacherId);
     }
+
 
     @Transactional
     public Student selectStudent(Long userId){
